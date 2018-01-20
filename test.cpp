bbefe2bddb04b3947977481782ef4360a45c25cd
Roman Alex <romalxr@gmail.com>
2018-01-20 09:00:28 +0000

    try with tests

--- test.cpp
+++ test.cpp
@@ -1,4 +1,12 @@
+//g++ test.cpp -lm -o  test -D_GLIBCXX_USE_CXX11_ABI=0 -lboost_unit_test_framework
+
 #define BOOST_TEST_MODULE test_version
+
+#define BOOST_TEST_MAIN
+
+#ifndef BOOST_TEST_DYN_LINK
+#define BOOST_TEST_DYN_LINK
+#endif
 
 #include "lib.h"
 
--- version.h
+++ version.h
@@ -1,3 +1,3 @@
 #pragma once
 
-#define PROJECT_VERSION_PATCH 1
+#define PROJECT_VERSION_PATCH 2
