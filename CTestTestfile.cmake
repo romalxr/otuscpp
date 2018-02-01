acbc7287d59ba4326f1409a978b87cdd9afccb86
Roman Alex <romalxr@gmail.com>
2018-02-01 09:36:26 +0000

    removed gitignored files

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/workspace/otus/otuscpp
-# Build directory: /home/ubuntu/workspace/otus/otuscpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_version "test_version")
